d63107741d8691de7df5e6e96b4d3edfc1911a0a
crold14 <carlos_roldan92@hotmail.com>
2022-04-12 11:54:34 +0200

    app.js con el core

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //1 paso: fichero que contiene todos los manejadores de la app (configuracion basica de nuestra app de express)
 
 const express = require('express');
+const cors = require('cors');
 
 // 2 paso: crear la app , crear la variable q nos permite recibir peticiones y generar respuestas
 
 const app = express();
 
 //config app expres
-
+app.use(cors());
 app.use(express.json()); //para poder recibir objetos a traves del body
 app.use(express.urlencoded({ extended: true })) // para poder pasar partes dinamicas en la url
 
