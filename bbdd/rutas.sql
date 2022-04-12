f07258326d40e452a5a29019f1182aa661507956
rtome14 <r.tome14@hotmail.com>
2022-04-12 11:59:46 +0200

    actualización base de datos

--- bbdd/rutas.sql
+++ bbdd/rutas.sql
@@ -36,7 +36,7 @@ CREATE TABLE `mapas` (
   `latfin` float NOT NULL,
   `longfin` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `mapas` (
 
 LOCK TABLES `mapas` WRITE;
 /*!40000 ALTER TABLE `mapas` DISABLE KEYS */;
-INSERT INTO `mapas` VALUES (1,'Quijorna','Salida de quijorna, pasando cruz verde, terminando en cebreros.',68,'linkimg','dificil',40.4398,-4.05659,40.468,-4.46652),(3,'Quijorna','Salida de quijorna, pasando cruz verde, terminando en cebreros.',68,'linkimg','dificil',40.4398,-4.05659,40.468,-4.46652),(4,'Atazar','Salida de San Agustín de Guadalix, pasando Torrelaguna, terminando en Atazar.',46,'linkimg','media',40.6933,-3.61616,40.9433,-3.47128);
+INSERT INTO `mapas` VALUES (1,'Quijorna','Salida de quijorna, pasando cruz verde, terminando en cebreros.',68,'linkimg','dificil',40.4398,-4.05659,40.468,-4.46652),(4,'Atazar','Salida de San Agustín de Guadalix, pasando Torrelaguna, terminando en Atazar.',46,'linkimg','media',40.6933,-3.61616,40.9433,-3.47128),(5,'Guadarrama','Salida en Soto del real, pasando por cercedilla y terminando en guadarrama',40,'linkimg','media',40.7585,-3.78385,40.6784,-4.08769),(7,'San Martín de Valdeiglesias','Salida en San Martín de Valdeiglesias, pasando por Burgohondo y terminando en Arenas de San pedro',103,'linkimg','facil',40.3731,-4.40228,40.2186,-5.08481),(9,'Chorreras de cabriel','Salida desde Cuenca, pasando por Carboneras y terminando en las chorreras de Cabriel.',78,'linkimg','dificil',40.0834,-2.14052,39.7169,-1.61554),(10,'Hoces del Duratón','Salida desde Segovia, pasando por Pedraza y terminando en las Hoces del río Duratón.',72,'linkimg','facil',40.9596,-4.11161,41.3422,-3.86992),(11,'Guadalupe','Salida desde Toledo, pasando por la nava de Ricomalillo y terminando en Guadalupe.',173,'linkimg','dificil',39.8725,-4.0277,39.4541,-5.32702);
 /*!40000 ALTER TABLE `mapas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +62,7 @@ CREATE TABLE `reseñas` (
   `coment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `val` enum('1','2','3','4','5') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,6 +71,7 @@ CREATE TABLE `reseñas` (
 
 LOCK TABLES `reseñas` WRITE;
 /*!40000 ALTER TABLE `reseñas` DISABLE KEYS */;
+INSERT INTO `reseñas` VALUES (1,'Divertida y bonita','Una ruta cerca de Madrid, con buenas curvas para aprender y disfrutar y rodeado de paisajes de la cercana sierra de gredos.','4');
 /*!40000 ALTER TABLE `reseñas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,4 +117,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-11 18:16:41
+-- Dump completed on 2022-04-12 11:48:35
